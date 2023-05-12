9d98c1c8cbbc5f393fd4e0ee2aae583a45049cc8
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-05-12 19:14:52 +0200

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2023_05_11_142711) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
-    t.integer "correct", default: 1
+    t.boolean "correct", default: false
     t.integer "question_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -46,14 +46,11 @@ ActiveRecord::Schema.define(version: 2023_05_11_142711) do
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
-    t.integer "test_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["test_id"], name: "index_users_on_test_id"
   end
 
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
-  add_foreign_key "users", "tests"
 end
