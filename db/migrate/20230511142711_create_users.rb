9c399b35490448f69436541af032ab835d59a3c9
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-05-12 19:14:49 +0200

    Update 20230511142711_create_users.rb

--- db/migrate/20230511142711_create_users.rb
+++ db/migrate/20230511142711_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       t.string :name, null:false
-      t.references :test, null: false, foreign_key: true
+     
 
       t.timestamps
     end
