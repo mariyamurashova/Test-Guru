0236af15b7e2eb82f9b898d897ac13195db9295a
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-05-14 22:28:57 +0200

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
-  belongs_to :test
+
+	def find_test_by_level(level, user)
+		Examination.where("user_id=?",user.id).find_each do |user_test|
+			Test.where("level=? AND id=?" ,level.to_i, user_test.test_id).find_each do |test|
+				puts test.title
+			end
+		end
+	end  
 end
