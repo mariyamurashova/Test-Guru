6d8aa38847d4ced723058c00b69a85da06003b51
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-05-14 22:28:53 +0200

    Update test.rb

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,9 @@
 class Test < ApplicationRecord
   belongs_to :category
+  def self.tests_names(category)
+    puts category
+    self.order(id: :desc).where(category_id: category.id).find_each do |test|
+      puts "#{test.id} - #{test.title}"
+    end
+  end
 end
