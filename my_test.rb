cd6b43ebf2123982d325e6c8de55f5c68f4d3e19
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-05-15 23:56:02 +0200

    Update my_test.rb

--- my_test.rb
+++ my_test.rb
@@ -1,9 +1,9 @@
- puts "Введите уровень сложности теста от 1 до 3"
- level = gets.chomp.to_i
- User.find_each do |user| 
-puts "#{user.name} прошел тесты:"
- puts user.find_test_by_level(level, user)
+puts "Введите уровень сложности теста от 1 до 3"
+  level = gets.chomp.to_i
+  User.find_each do |user| 
+ puts "#{user.name} прошел тесты:"
+  puts user.find_test_by_level(level, user)
+end
+  category=Category.where(title: :English)
+  puts Test.tests_names(category[0])
 
- category=Category.where(title: :English)
- Test.tests_names(category[0])
- end
