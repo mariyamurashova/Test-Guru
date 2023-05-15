0ae82b45561480ad55ab78520452a56610ad40e8
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-05-15 23:55:32 +0200

    Update test.rb

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,8 +2,6 @@ class Test < ApplicationRecord
   belongs_to :category
   
   def self.tests_names(category)
-    self.order(id: :desc).where(category_id: category.id).find_each do |test|
-      puts "#{test.id} - #{test.title}"
-    end
+    self.order(id: :desc).where(category_id: category.id).pluck
   end
 end
