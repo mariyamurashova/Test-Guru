8e869f2e738895ab29100eeb26c6834e9cab01c2
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-05-16 20:44:57 +0200

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
 
-	def find_test_by_level(level, user)
-	result=Test.joins("INNER JOIN examinations ON tests.id=test_id").
-	joins("INNER JOIN users ON users.id=user_id").
-	where(tests: {level:  level}, users: {id: user.id}).pluck(:title)
+	def find_test_by_level(level)
+		Test.joins("INNER JOIN examinations ON tests.id = examinations.test_id").where(examinations: {user_id: id}, level:  level,).pluck(:title)
 	end
 end
