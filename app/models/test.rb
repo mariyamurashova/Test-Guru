0350e68c54cb0e133bab503b1f1b80cbdd16f7a3
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-05-17 23:37:28 +0200

    Update test.rb

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,9 @@
 class Test < ApplicationRecord
   belongs_to :category
-  
+  belongs_to :user
+  has_many :questions
+  has_many :tests_users
+  has_many :users, through: :tests_users
   def self.tests_names(category)
     self.order(id: :desc).where(category: category).pluck(:title)
   end
