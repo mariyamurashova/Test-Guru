76159c4e502b73eb31576bd2000dcb472d9c88e2
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-05-17 23:37:41 +0200

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_14_095626) do
+ActiveRecord::Schema.define(version: 2023_05_17_210304) do
+
+  create_table "add_user_to_tests", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_add_user_to_tests_on_user_id"
+  end
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -53,12 +60,19 @@ ActiveRecord::Schema.define(version: 2023_05_14_095626) do
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
+  create_table "tests_users", id: false, force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "test_id", null: false
+    t.index ["user_id", "test_id"], name: "index_tests_users_on_user_id_and_test_id", unique: true
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "add_user_to_tests", "users"
   add_foreign_key "answers", "questions"
   add_foreign_key "examinations", "tests"
   add_foreign_key "examinations", "users"
