39787a9741fc2d2706a3eb9e887165b727d3de13
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-05-19 23:44:26 +0200

    Delete 20230517202138_create_add_user_to_tests.rb

--- db/migrate/20230517202138_create_add_user_to_tests.rb
+++
@@ -1,9 +0,0 @@
-class CreateAddUserToTests < ActiveRecord::Migration[6.1]
-  def change
-    create_table :add_user_to_tests do |t|
-      t.references :user, null: false, foreign_key: true
-
-      t.timestamps
-    end
-  end
-end
