105e442fcdfb3636414126c90cd1fb23a8bd0658
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-05-19 21:12:50 +0200

    Update test.rb

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,10 +1,11 @@
 class Test < ApplicationRecord
   belongs_to :category
-  belongs_to :user
+  belongs_to :author, class_name: "User"
   has_many :questions
   has_many :tests_users
   has_many :users, through: :tests_users
   def self.tests_names(category)
-    self.order(id: :desc).where(category: category).pluck(:title)
-  end
+    #self.joins("INNER JOIN categories ON category_id=categories.id").where(categories:{title: category}).order(id: :desc).pluck(:title)
+    self.joins(:category).where(categories:{title: category}).order(id: :desc).pluck(:title)
+end
 end
