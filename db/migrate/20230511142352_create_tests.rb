f747e549fd54327b6b3bc3f5984f1dcbe726f9aa
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-05-19 23:39:49 +0200

    Update 20230511142352_create_tests.rb

--- db/migrate/20230511142352_create_tests.rb
+++ db/migrate/20230511142352_create_tests.rb
@@ -1,7 +1,6 @@
 class CreateTests < ActiveRecord::Migration[6.1]
   def change
     create_table :tests do |t|
-      t.references :user, foreign_key: {to_table: :users}
       t.string :title, null:false
       t.integer :level, default: 1
       t.references :category, null: false, foreign_key: true
