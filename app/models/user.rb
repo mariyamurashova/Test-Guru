95dfb554040682f8152e3eb0f1ebaccc21922560
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-05-21 19:56:27 +0200

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-	has_many :test_created, class_name: "Test"
-	has_many :tests_users
-	has_many :tests, through: :tests_users
+  has_many :test_created, class_name: "Test"
+  has_many :tests_users
+  has_many :tests, through: :tests_users
 
-	def find_test_by_level(level)
-		Test.joins(:tests_users).where(tests_users: {user_id: id}, level:  level).pluck(:title)
-	end
+  def find_test_by_level(level)
+	 Test.joins(:tests_users).where(tests_users: {user_id: id}, level:  level).pluck(:title)
+  end
 end
