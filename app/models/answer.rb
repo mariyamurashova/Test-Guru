b30ba28e94cf1766e3af0504a404de89fd251c8f
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-05-25 00:04:19 +0200

    Update answer.rb

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,11 @@
 class Answer < ApplicationRecord
   belongs_to :question
+  validates :body, presence: true
+  validate :answers_amount 
+  scope :just_correct, -> {where correct: 1}
+
+def answers_amount 
+  errors.add :base, :invalid, message: "More than 4 answers" if Answer.where(question_id: self.question_id).length > 4
+end
+
 end
