17054c2747b7b66fee654c1da8ef13f36afa2b80
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-05-25 00:04:32 +0200

    Update test.rb

--- app/models/test.rb
+++ app/models/test.rb
@@ -4,7 +4,26 @@ class Test < ApplicationRecord
   has_many :questions
   has_many :tests_users
   has_many :users, through: :tests_users
-  def self.tests_names(category)
-    self.joins(:category).where(categories:{title: category}).order(id: :desc).pluck(:title)
+
+  validates :title, uniqueness: { scope: :level } 
+  validates :level, numericality: {only_integer: true, greater_than_or_equal_to: 0} 
+  validates :title, presence: true
+  scope :tests_names, -> (category) { joins(:category).select_categories(category).order(id: :desc).pluck(:title)}
+  scope :difficulty, -> (level) {  if level.include?'easy'  
+                           where level: 0..1 
+                          else
+                          if level.include?'medium'
+                            where level: 2..4
+                          else
+                          if level.include?'hard'
+                            where level: 5..Float::INFINITY
+                          end
+                          end
+                        end
+                        }
+  
+
+  def self.select_categories(category)
+    where(categories:{title: category})
   end
 end
