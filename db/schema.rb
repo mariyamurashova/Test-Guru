f73b5a12b86d938b216f14e7219ae0d54a236e49
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-05-25 00:05:31 +0200

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_18_145831) do
+ActiveRecord::Schema.define(version: 2023_05_24_213954) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2023_05_18_145831) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["title", "level"], name: "index_title_level", unique: true
     t.index ["user_id"], name: "index_tests_on_user_id"
   end
 
@@ -54,6 +55,7 @@ ActiveRecord::Schema.define(version: 2023_05_18_145831) do
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
+    t.string "email", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
