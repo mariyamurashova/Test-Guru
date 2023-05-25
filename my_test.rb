68bd7e460d0f033602a165bbd34d240420f5b7ff
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-05-25 21:00:14 +0200

    Update my_test.rb

--- my_test.rb
+++ my_test.rb
@@ -3,7 +3,7 @@ category=Category.new(title: :English)
 category.save
 user=User.new(name: 'Ivanov', email: '123@com')
 user.save
-test=user.tests.new(title: 'New Question3', level: 2, category: category, author: user)
+test=user.tests.new(title: 'New Question4', level: 2, category: category, author: user)
 test.save!
 question=test.questions.new(body:'Question?')
 question.save!
@@ -11,3 +11,4 @@ answer=question.answers.new(body: 'First')
 answer.save!
 answer=question.answers.new(body: 'Second')
 answer.save!
+
