cb1ed758a73b06c74244dcf66dc87830d289afd3
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-05-31 10:19:10 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  #root 'test_questions#index'
-
   resources :tests  do 
     resources :questions#, shallow: true 
   end
