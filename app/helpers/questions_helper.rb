b27a26c67f97338f617f505f40b32872410d17c6
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-06-06 21:47:30 +0200

    Update questions_helper.rb

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,25 @@
 module QuestionsHelper
+  
+  def question_header
+    #byebug
+    test = Test.find(@question.test_id)
+    if params[:action] == "edit" 
+      "Edit #{test.title} Question"
+    else
+      "Create New Question for Test - #{test.title}"
+    end
+  end
+
+  def curent_year
+    time = Time.new
+    "#{time.year} г."
+  end
+
+  def github_url(author, repo)
+   "https://github.com/#{author}/#{repo}"
+  end
+
+  def link_back
+   "/tests/#{@question[:test_id]}/questions"
+  end
 end
