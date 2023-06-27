5f5008f4f4bdadbdd4f47da4e8335a62dd5ec50c
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-06-27 23:37:36 +0200

    Create test_passage.rb

---
+++ app/models/test_passage.rb
@@ -0,0 +1,68 @@
+class TestPassage < ApplicationRecord
+  belongs_to :user
+  belongs_to :test
+  belongs_to :current_question, class_name: "Question", optional: true
+
+  before_validation :before_validation_set_first_question, on: :create
+  before_validation :before_validation_set_next_question, on: :update
+
+  def completed?
+    current_question.nil?
+  end
+
+  def accept!(answer_ids)
+
+    if correct_answer?(answer_ids)
+      self.correct_questions += 1
+
+    end
+    save!
+  end
+
+  def success_rate
+    self.correct_questions*100/test.questions.count
+
+  end
+
+  def success?
+    self.success_rate>85
+  end
+
+
+  def question_number
+    test.questions.order(:id).where('id <= ?', current_question.id).count
+  end
+
+  def questions_amount
+    test.questions.length
+  end
+
+
+  private
+
+
+  def before_validation_set_first_question
+    self.current_question = test.questions.first if test.present?
+  end
+
+  def before_validation_set_next_question
+    self.current_question = next_question 
+  end
+
+  def correct_answer?(answer_ids)
+     correct_answers.ids.sort == answer_ids.map(&:to_i).sort
+  end
+
+  def correct_answers
+    current_question.answers.correct_answer 
+  end
+
+  def next_question
+    test.questions.order(:id).where('id > ?', current_question.id).first
+  end
+
+  def before_save_set_next_question  
+    self.current_question = next_question 
+  end
+
+end
