2c10ac46a6da476e0311ea10595d59c7f2fc9481
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-06-27 23:37:34 +0200

    Update test.rb

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,8 +2,8 @@ class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: "User", foreign_key: :user_id
   has_many :questions
-  has_many :tests_users
-  has_many :users, through: :tests_users
+  has_many :test_passages
+  has_many :users, through: :test_passages
 
   validates :title, uniqueness: { scope: :level } 
   validates :level, numericality: {only_integer: true, greater_than_or_equal_to: 0} 
