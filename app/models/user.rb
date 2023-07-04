a98b86482dd9741bafb1ea122c3451b021935621
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-07-05 01:03:08 +0200

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,16 @@
+require 'digest/sha1'
+
 class User < ApplicationRecord
-  has_many :test_created, class_name: "Test"
+
+  EMAIL_REGEX = /[A-Za-z0-9][A-Za-z0-9\.-_]*[A-Za-z0-9]*@([A-Za-z0-9]+([A-Za-z0-9-]*[A-Za-z0-9]+)*\.)+[A-Za-z]*$\z/i
+
+  has_many :test_created, class_name: "Test", foreign_key: :user_id
   has_many :test_passages
   has_many :tests, through: :test_passages
-  validates :name, :email, presence: true
 
+  has_secure_password
+
+  validates :email, uniqueness: true, format: { with: EMAIL_REGEX, message: "wrong email's format" }
 
   def find_test_by_level(level)
 	 Test.joins(:tests_users).where(tests_users: {user_id: id}, level:  level).pluck(:title)
@@ -12,4 +19,5 @@ class User < ApplicationRecord
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id) 
   end
+
 end
