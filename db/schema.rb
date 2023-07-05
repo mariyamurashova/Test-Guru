be15bd4293be5851487fa99a615532c716a73418
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-07-05 22:00:43 +0200

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_29_060502) do
+ActiveRecord::Schema.define(version: 2023_07_05_194446) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2023_06_29_060502) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "email"
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "answers", "questions"
