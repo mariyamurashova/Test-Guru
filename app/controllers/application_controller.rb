756d704a135ef0735f07582924ab8a9be783c3e3
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-07-05 22:00:17 +0200

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
 
   def authenticate_user!
     unless current_user
+      session['previous_url'] = request.referrer
       redirect_to login_path, alert: 'Are you a Guru? Verify your Email and Password please'
     end
   end
