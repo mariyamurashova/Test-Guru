73c181a12d7f87bfa26527484cecf34a43558be9
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-07-05 22:00:33 +0200

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,13 @@ require 'digest/sha1'
 
 class User < ApplicationRecord
 
-  EMAIL_REGEX = /[A-Za-z0-9][A-Za-z0-9\.-_]*[A-Za-z0-9]*@([A-Za-z0-9]+([A-Za-z0-9-]*[A-Za-z0-9]+)*\.)+[A-Za-z]*$\z/i
-
   has_many :test_created, class_name: "Test", foreign_key: :user_id
   has_many :test_passages
   has_many :tests, through: :test_passages
 
   has_secure_password
 
-  validates :email, uniqueness: true, format: { with: EMAIL_REGEX, message: "wrong email's format" }
+  validates :email, uniqueness: true, format:  {with: URI::MailTo::EMAIL_REGEXP}
 
   def find_test_by_level(level)
 	 Test.joins(:tests_users).where(tests_users: {user_id: id}, level:  level).pluck(:title)
