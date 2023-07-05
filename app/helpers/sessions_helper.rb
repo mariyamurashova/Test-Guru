25d8d647e6220ac94644962ebf8ec5a32275472a
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-07-05 22:00:31 +0200

    Update sessions_helper.rb

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,8 +3,10 @@ module SessionsHelper
   def flash_message
 
    if flash[:alert] 
-      content_tag :p, flash[:alert], class: 'flash alert'
+    flash.each do |key, msg|
+      return content_tag :div, msg, :id => key, :class => 'flash alert'
     end
+   end
   end
 
 end
