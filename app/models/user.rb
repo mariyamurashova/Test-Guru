8f6cb7afbebb3336e6f4ffff52edab64a79ceb2a
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-07-17 02:27:13 +0200

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,21 @@
-require 'digest/sha1'
-
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, 
+         :registerable,
+         :recoverable, 
+         :rememberable, 
+         :validatable,
+         :confirmable
 
   has_many :test_created, class_name: "Test", foreign_key: :user_id
   has_many :test_passages
   has_many :tests, through: :test_passages
 
-  has_secure_password
+  #has_secure_password
 
   validates :email, uniqueness: true, format:  {with: URI::MailTo::EMAIL_REGEXP}
+  validates :first_name, :last_name, presence: true
 
   def find_test_by_level(level)
 	 Test.joins(:tests_users).where(tests_users: {user_id: id}, level:  level).pluck(:title)
