27b4de31808f5a741b35c05b97cace74f783d0a9
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-07-17 02:31:14 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,9 @@ Rails.application.routes.draw do
 
   root 'tests#index'
 
-  get :signup, to: 'users#new'
-  get :login, to: 'sessions#new'
+  devise_for :users, path: :gurus, path_names: { sign_in: :login, sign_out: :logout }
 
-  resources :users, only: :create
-  resources :sessions, only: [:new, :create, :destroy]
-
-  resources :tests  do 
+  resources :tests, only: :index do 
     resources :questions, shallow: true do 
       resources :answers, shallow: true
     end
@@ -20,6 +16,14 @@ end
   resources :test_passages do
     member  do
       get :result
+    end
+  end
+
+  namespace :admin do 
+    resources :tests do
+      resources :questions, shallow: true  do 
+       resources :answers, shallow: true
+      end
     end
   end
     
