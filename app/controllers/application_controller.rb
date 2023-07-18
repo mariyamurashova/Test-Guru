f87f7c9062b214f8ed82a1084806639857c2b86f
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-07-18 14:36:41 +0200

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
-  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:create], if: :devise_controller?
 
   def after_sign_in_path_for(user)
-    
-    if current_user.is_a?(Admin) 
-      admin_tests_path
+byebug
+    if user.admin? 
+     admin_tests_path
     else
-      flash.notice = "Привет,#{current_user.first_name}"
+      flash.notice = "Привет,#{user.first_name}"       
       root_path
     end
 
