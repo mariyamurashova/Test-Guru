c9025657b6b5487ecaa1ee5b1c2a48d6bab45bb7
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-08-06 01:02:10 +0200

    Update test_passages_controller.rb

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -1,7 +1,7 @@
 class TestPassagesController < ApplicationController
 
   before_action :authenticate_user!
-  before_action :set_test_passage, only: %i[show result update]
+  before_action :set_test_passage, only: %i[show result update gist]
   
   def show
   end
@@ -20,7 +20,26 @@ class TestPassagesController < ApplicationController
     end
   end
 
+  def gist
+    @result = GistQuestionService.new(@test_passage.current_question).call 
+   
+    save_gist
+  
+    if response.status == 200
+      flash_options = {notice: "#{t('.success')} #{view_context.link_to t('.link'), @result.html_url}"}
+    else
+      flash_options = {alert: "#{response.status} #{t('.failure')}"}
+    end
+
+    redirect_to @test_passage, flash_options
+  end
+
   private
+
+  def save_gist
+    @gist = @test_passage.user.gists.new(:question_id=>@test_passage.current_question_id, :gist_url=>@result.html_url)
+    @gist.save!
+  end
 
   def set_test_passage
     @test_passage = TestPassage.find(params[:id])
