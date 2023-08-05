53d3c019e9dcee0a14b6058758e1f1c32d2f944b
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-08-06 01:02:17 +0200

    Update question.rb

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,7 @@ class Question < ApplicationRecord
   belongs_to :test
   has_many :answers
   has_many :current_questions, class_name: 'TestPassage'
+  has_many :gists
   validates :body, presence: true
   
 end
