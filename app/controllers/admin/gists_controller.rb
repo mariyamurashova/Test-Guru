427394149da44bfd43e2ef858c44c6c55110fec8
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-08-08 01:56:33 +0200

    Update gists_controller.rb

--- app/controllers/admin/gists_controller.rb
+++ app/controllers/admin/gists_controller.rb
@@ -1,11 +1,6 @@
 class Admin::GistsController < Admin::BaseController
 
-  def create(test_passage, result)
-    @gist = test_passage.user.gists.new(:question_id=>test_passage.current_question_id, :gist_url=>result.html_url)
-    @gist.save!
-  end
-
-  def show
+  def index
     @gists = Gist.all
   end
 
