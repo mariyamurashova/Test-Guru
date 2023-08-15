fd9ef9a566fd20899b7a3fbac3009588553ef86c
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-08-15 11:21:49 +0300

    Update check_password.js

--- app/javascript/packs/utilities/check_password.js
+++ app/javascript/packs/utilities/check_password.js
@@ -3,7 +3,7 @@ document.addEventListener('turbolinks:load', function() {
 document.getElementById('user_password').addEventListener("input", function (e) {
  var userPassword = e.target.value
 
-document.getElementById('user_password_confirmation').addEventListener("input",function (e) {
+document.getElementById('user_password_confirmation').addEventListener("input",function (e) {   
        
   if (userPassword == e.target.value) {
     document.querySelector('.octicon-check').classList.remove('hide')
@@ -13,6 +13,8 @@ document.getElementById('user_password_confirmation').addEventListener("input",f
     document.querySelector('.octicon-alert').classList.remove('hide')
     document.querySelector('.octicon-check').classList.add('hide')
       }
+
+  if (e.target.value == '') {document.querySelector('.octicon-alert').classList.add('hide')}   
     })
   })
 })
