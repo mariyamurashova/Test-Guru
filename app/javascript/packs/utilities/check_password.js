63366b40a33ec27891cb7f952dff1e173e3dadaf
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-08-16 17:32:56 +0300

    Update check_password.js

--- app/javascript/packs/utilities/check_password.js
+++ app/javascript/packs/utilities/check_password.js
@@ -1,21 +1,26 @@
 document.addEventListener('turbolinks:load', function() {
+ var userPassword
 
 document.getElementById('user_password').addEventListener("input", function (e) {
- var userPassword = e.target.value
+  userPassword = e.target.value 
 
-document.getElementById('user_password_confirmation').addEventListener("input",function (e) {   
-       
-  if (userPassword == e.target.value) {
+document.getElementById('user_password_confirmation').addEventListener("input",function (e) {  
+
+if (e.target.value == '') {return(document.querySelector('.octicon-alert').classList.add('hide'))}  
+
+comparePasswords (e.target.value)
+    })
+  })
+
+function comparePasswords(passwordConfirmation){
+  
+  if (userPassword == passwordConfirmation) {
     document.querySelector('.octicon-check').classList.remove('hide')
     document.querySelector('.octicon-alert').classList.add('hide')
     }
   else {
     document.querySelector('.octicon-alert').classList.remove('hide')
     document.querySelector('.octicon-check').classList.add('hide')
-      }
-
-  if (e.target.value == '') {document.querySelector('.octicon-alert').classList.add('hide')}   
-    })
-  })
+    }
+  }
 })
-
