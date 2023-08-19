66768dcfdc1aa18d16a44359fa066e81f43727a8
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-08-20 01:10:17 +0300

    Create progress_bar.js

---
+++ app/javascript/packs/utilities/progress_bar.js
@@ -0,0 +1,12 @@
+document.addEventListener('turbolinks:load', function() {
+
+  if (document.querySelector('.progress-bar')) {
+    var progressBar = document.querySelector('.progress-bar')
+    var questionNumber = Number(progressBar.dataset.questionNumber)
+    var questionsAmount = Number(progressBar.dataset.questionsAmount)
+    var countProgress = (questionNumber / questionsAmount)*100
+   
+     $('.progress-bar').css('width', countProgress+'%').attr('aria-valuenow', countProgress);  
+  }
+
+})
