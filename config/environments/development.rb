78f973f9597a5a9b63d9f8a295d747f6c5818de5
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-08-29 15:16:26 +0300

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,16 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  #config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.smtp_settings = {
+  #address:              'smtp.gmail.com',
+  #port:                 587,
+  #domain:               'gmail.com',
+  #user_name:            'mariya09.2009@gmail.com',
+  #password:             'Lionking26',
+  #authentication:       'plain',
+  #enable_starttls_auto: true  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
