9bc9e6cd893fa6fb38ebaea7843b878def660e96
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-08-30 17:30:09 +0300

    Update test_passages_controller.rb

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -2,7 +2,7 @@ class TestPassagesController < ApplicationController
 
   before_action :authenticate_user!
   before_action :set_test_passage, only: %i[show result update]
-  
+
   def show
   end
 
@@ -10,20 +10,30 @@ class TestPassagesController < ApplicationController
   end
     
   def update
-    @test_passage.accept!(params[:answer_ids])
 
-    if @test_passage.completed?
-      TestsMailer.completed_test(@test_passage).deliver_now
-      redirect_to result_test_passage_path(@test_passage)
+    if params[:answer_ids]!=nil
+      @test_passage.accept!(params[:answer_ids])
+        if @test_passage.completed?
+          TestsMailer.completed_test(@test_passage).deliver_now
+          redirect_to result_test_passage_path(@test_passage)
+        else
+          render :show
+        end
     else
-      render :show
+      redirect_to test_passage_path, notice: t('.give_answer')
     end
+ 
   end
 
   private
 
   def set_test_passage
     @test_passage = TestPassage.find(params[:id])
+  end
+
+  def get_answer?
+    if params[:answer_ids]!=nil
+    end
   end
 
 end
