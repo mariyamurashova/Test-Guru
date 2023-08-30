e867e5507ee96fd061241672251f7f05a58af0d1
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-08-30 17:29:55 +0300

    Update contacts_controller.rb

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,14 +6,11 @@ class ContactsController < ApplicationController
 
   def create
     contact = params.permit(:name, :email, :message)
-    #byebug
       if ContactMailer.send_mail(contact).deliver_now
         redirect_to root_path, notice: t('.success')
       else
         render :new
       end
   end
-
-  private
 
 end
