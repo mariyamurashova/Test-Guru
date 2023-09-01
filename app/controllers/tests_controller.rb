e23dc28de5298d5b52a4e6e64ceffbf9ace02b30
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-09-01 12:29:17 +0300

    Update tests_controller.rb

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -4,16 +4,13 @@ class TestsController < ApplicationController
   before_action :set_test, only: %i[ start ]
 
   def index
-    @tests = Test.all  
+    @tests = Test.where("redy_for_publication=true")
+    
   end
 
   def start
-    if @test.questions.presence
       current_user.tests.push(@test)
       redirect_to current_user.test_passage(@test)
-    else
-      redirect_to root_path, notice: "Test hasn't questions at the moment"
-    end
   end
 
   private
