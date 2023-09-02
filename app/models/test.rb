384550fcc9e7a0ef2f10e8b47feb221fc3a21b1c
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-09-02 15:53:44 +0300

    Update test.rb

--- app/models/test.rb
+++ app/models/test.rb
@@ -11,6 +11,7 @@ class Test < ApplicationRecord
   validates :title, presence: true
 
   scope :join_category, -> (category) { joins(:category).where(categories: {title: category})}
+  scope :published, -> { where(redy_for_publication: true) }
  
   scope :easy, -> { where(level: 0..1) }
   scope :mmiddle, -> { where(level: 2..4) }
