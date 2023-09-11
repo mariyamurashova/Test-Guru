54bdf44c3ead1917bf161ee84f81c5acb346c604
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-09-11 13:37:13 +0300

    Update 20230905080238_create_badges.rb

--- db/migrate/20230905080238_create_badges.rb
+++ db/migrate/20230905080238_create_badges.rb
@@ -1,8 +1,8 @@
 class CreateBadges < ActiveRecord::Migration[6.1]
   def change
     create_table :badges do |t|
-      t.string :title
-      t.string :image
+      t.string :title, null: false
+      t.string :image, null: false
       t.timestamps
     end
   end
