e5f0687d6ea4e89a66013f897bec127cbe2c73c5
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-09-16 10:57:57 +0300

    Update 20230911192417_drop_table_badges_users.rb

--- db/migrate/20230911192417_drop_table_badges_users.rb
+++ db/migrate/20230911192417_drop_table_badges_users.rb
@@ -1,5 +1,12 @@
 class DropTableBadgesUsers < ActiveRecord::Migration[6.1]
   def up
-    drop_table :badges_users, if_exists: true
+    #drop_table :badges_users, if_exists: true
+  end
+
+  def down
+    create_join_table :badges, :users do |t|
+      t.index [:badge_id, :user_id]
+      t.index [:user_id, :badge_id]
+    end
   end
 end
