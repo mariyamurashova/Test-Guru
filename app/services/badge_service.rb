1d2ecd54dc19f09beeda222358056f0536b405ef
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-09-17 16:04:44 +0300

    Update badge_service.rb

--- app/services/badge_service.rb
+++ app/services/badge_service.rb
@@ -14,22 +14,31 @@ class BadgeService
   end
 
   def first_attempt_aword?(badge)
-   TestPassage.where(user_id: @user.id, test_id: @test_passage.test_id).count == 1 
+   TestPassage.with_result_success.where(user_id: @user.id, test_id: @test_passage.test_id).count == 1 
   end
 
   def category_aword?(badge)
-    ids=Test.join_category(badge.rule_value)
-    tests_pass_success = TestPassage.with_result_success.where(test_id: ids).select(:test_id).uniq.count 
-    tests_pass_success == Test.join_category(badge.rule_value).count   
+   tests_pass_success(tests_with_category(badge.rule_value))  ==  tests_with_category(badge.rule_value).count  
   end
 
   def level_aword?(badge)
-    tests_pass_success = TestPassage.with_result_success.where(user_id: @user.id).joins(:test).where(tests: {level:badge.rule_value}).select(:test_id).uniq.count
-    tests_pass_success == Test.tests_with_level(badge.rule_value).count  
+    tests_pass_success(tests_with_level(badge.rule_value.to_i)) == tests_with_level(badge.rule_value.to_i).count  
   end
 
   def recieved_earlier?(badge)
-    @user.badges.include?(badge)
+    @user.badges.include?(badge) && badge.rule != "first_attempt"
+  end
+
+  def tests_with_category(rule_value)
+    Test.join_category(rule_value).pluck(:id)
+  end
+
+  def tests_with_level(rule_value)
+    Test.tests_with_level(rule_value).pluck(:id)
+  end
+
+  def tests_pass_success(tests_ids)
+    TestPassage.with_result_success.where(user_id: @user.id, test_id: tests_ids).pluck(:test_id).uniq.count
   end
 
 end
